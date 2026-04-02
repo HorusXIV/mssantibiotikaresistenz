175f98f44aa74c69f192b78462f4972c1c1733ce
Lukas Breiter <lukas.breiter@students.fhnw.ch>
2026-04-02 16:59:32 +0200

    fixed wrong image name

--- slurm_runner.sh
+++ slurm_runner.sh
@@ -4,6 +4,8 @@
 #SBATCH --job-name=MSS_Simulation
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=64
+#SBATCH --output=/home2/lukas.breiter/CIC_slurm_le4/MSS/logs/%x-%j.out
+#SBATCH --error=/home2/lukas.breiter/CIC_slurm_le4/MSS/logs/%x-%j.err
 
 # Absolute path to your project on the host
 PROJECT_DIR=/home2/lukas.breiter/CIC_slurm_le4/MSS
@@ -11,11 +13,13 @@ PROJECT_DIR=/home2/lukas.breiter/CIC_slurm_le4/MSS
 # Where to mount it inside the container
 WORKDIR=/workspace
 
+mkdir -p "${PROJECT_DIR}/logs"
+
 singularity exec \
-     --bind ${PROJECT_DIR}:${WORKDIR} \
-     python_poetry.sif \
-     bash -lc "\
-       cd ${WORKDIR} && \
-       uv sync --no-interaction && \
-       uv run python run_coupled_simulation.py --config shared/config_abx.yml \
-     "
+  --bind ${PROJECT_DIR}:${WORKDIR} \
+  containers/mss_image.sif \
+  bash -lc "\
+    cd ${WORKDIR} && \
+    uv sync && \
+    uv run python run_coupled_simulation.py --config shared/config_abx.yml \
+  "
